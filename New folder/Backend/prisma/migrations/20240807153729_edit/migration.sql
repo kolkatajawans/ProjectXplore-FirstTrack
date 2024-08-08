6616598ff51b10947c5add433c7c9a773a8665cf
collegeparam <138299927+param2610-cloud@users.noreply.github.com>
2024-08-08 22:53:21 +0530

    illusion gone

--- New folder/Backend/prisma/migrations/20240807153729_edit/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Request" ADD COLUMN     "roomId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "Request" ADD CONSTRAINT "Request_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- New folder/Frontend/src/lib/atoms/userAtom.ts
+++
@@ -1,4 +0,0 @@
-// atoms/userAtom.ts
-import { atom } from 'jotai';
-
-export const userAtom = atom<{ uid: string } | null>(null);
